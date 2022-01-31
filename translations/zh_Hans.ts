45dd7fc6c5b2b1b40a4174b3d9dc4bcad485c2a8
Calcitem <calcitem@outlook.com>
2022-01-31 17:29:59 +0000

    Translated using Weblate (Chinese (Simplified))
    Currently translated at 100.0% (43 of 43 strings)
    
    Translation: Deepines Store/Deepines Store
    Translate-URL: https://hosted.weblate.org/projects/deepines-store/deepines-store/zh_Hans/

--- translations/zh_Hans.ts
+++ translations/zh_Hans.ts
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0" language="zh_Hans" sourcelanguage="">
+<!DOCTYPE TS>
+<TS version="2.0" language="zh_Hans" sourcelanguage="">
 <context>
     <name>Ui_DialogInstall</name>
     <message>
@@ -7,17 +8,19 @@
         <source>
 Installing {item}
 </source>
-        <translation type="unfinished"></translation>
+        <translation>
+正在安装 {item}
+</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="144"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>关闭</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="143"/>
         <source>Retry</source>
-        <translation type="unfinished"></translation>
+        <translation>重试</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="158"/>
@@ -27,19 +30,26 @@ The installation system is locked by
 another installation or upgrade process, 
 wait until it finishes and click Retry. 
 If you use Synaptic, make sure it is closed.</source>
-        <translation type="unfinished"></translation>
+        <translation>
+
+安装系统被另一个
+安装或升级过程锁定，
+等到它完成，然后单击“重试”。
+如果您使用新立得，请确保它已关闭。</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="142"/>
         <source>Install</source>
-        <translation type="unfinished"></translation>
+        <translation>安装</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="146"/>
         <source>
 All processes have been completed.
 </source>
-        <translation type="unfinished"></translation>
+        <translation>
+所有过程都已完成。
+</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="145"/>
@@ -47,12 +57,15 @@ All processes have been completed.
 
 Warning: do not close the window, interrupting the installation may damage your system.
 </source>
-        <translation type="unfinished"></translation>
+        <translation>
+
+警告：不要关闭窗口，中断安装可能会损坏您的系统。
+</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="139"/>
         <source>Installation process - Deepines Store</source>
-        <translation type="unfinished"></translation>
+        <translation>安装过程 - 深度商店</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="162"/>
@@ -60,14 +73,19 @@ Warning: do not close the window, interrupting the installation may damage your 
 The database is being updated.
 
 </source>
-        <translation type="unfinished"></translation>
+        <translation>
+数据库正在更新。
+
+</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="148"/>
         <source>
 The installation of {item} is finished.
 </source>
-        <translation type="unfinished"></translation>
+        <translation>
+{item} 的安装完成。
+</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="151"/>
@@ -77,19 +95,26 @@ The network connection has failed and the installation has not been completed.
 Check that your computer is connected to the Internet and click Retry.
 If the problem persists, send a report to t.me/deepinenespanol.
 </source>
-        <translation type="unfinished"></translation>
+        <translation>
+
+网络连接失败，安装尚未完成。
+检查您的计算机是否已连接到 Internet，然后单击“重试”。
+如果问题仍然存在，请向 t.me/deepinenespanol 发送报告。
+</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="140"/>
         <source>{app_count} app has been selected for installation:
 </source>
-        <translation type="unfinished"></translation>
+        <translation>已选择 {app_count} 个应用进行安装：
+</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="141"/>
         <source>{app_count} apps have been selected for installation:
 </source>
-        <translation type="unfinished"></translation>
+        <translation>已选择 {app_count} 个应用进行安装：
+</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="149"/>
@@ -98,7 +123,11 @@ If the problem persists, send a report to t.me/deepinenespanol.
 An error has occurred, please try again.
 If the problem persists, contact the administrator.
 </source>
-        <translation type="unfinished"></translation>
+        <translation>
+
+发生了错误。请再试一次。
+如果问题仍然存在，请联系管理员。
+</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="154"/>
@@ -109,7 +138,13 @@ they depend on other packages that cannot be installed.
 
 You can look for help in our forum at deepinenespanol.org/comunidad
 or in our Telegram group t.me/deepinenespanol.</source>
-        <translation type="unfinished"></translation>
+        <translation>
+
+无法安装一个或多个应用程序，因为
+它们依赖于其他无法安装的软件包。
+
+您可以在我们的论坛 deepinenespanol.org/comunidad 
+或在我们的 Telegram 组 t.me/deepinenespanol 中寻求帮助。</translation>
     </message>
 </context>
 <context>
@@ -117,12 +152,12 @@ or in our Telegram group t.me/deepinenespanol.</source>
     <message>
         <location filename="../deepinesStore/cardg.py" line="69"/>
         <source>Install</source>
-        <translation type="unfinished"></translation>
+        <translation>安装</translation>
     </message>
     <message>
         <location filename="../deepinesStore/cardg.py" line="68"/>
         <source>Card</source>
-        <translation type="unfinished"></translation>
+        <translation>卡片</translation>
     </message>
 </context>
 <context>
@@ -130,133 +165,134 @@ or in our Telegram group t.me/deepinenespanol.</source>
     <message>
         <location filename="../deepinesStore/maing.py" line="472"/>
         <source>Home</source>
-        <translation type="unfinished"></translation>
+        <translation>首页</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="482"/>
         <source>Games</source>
-        <translation type="unfinished"></translation>
+        <translation>游戏</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="490"/>
         <source>Other</source>
-        <translation type="unfinished"></translation>
+        <translation>其他</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="468"/>
         <source>Install</source>
-        <translation type="unfinished"></translation>
+        <translation>安装</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="493"/>
         <source>About 
 Version: {version}</source>
-        <translation type="unfinished"></translation>
+        <translation>关于
+版本：{version}</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="486"/>
         <source>Development</source>
-        <translation type="unfinished"></translation>
+        <translation>开发</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="492"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>搜索</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="476"/>
         <source>Internet</source>
-        <translation type="unfinished"></translation>
+        <translation>互联网</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="488"/>
         <source>System</source>
-        <translation type="unfinished"></translation>
+        <translation>系统</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="480"/>
         <source>Graphics</source>
-        <translation type="unfinished"></translation>
+        <translation>图形</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="465"/>
         <source>About us</source>
-        <translation type="unfinished"></translation>
+        <translation>关于我们</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="478"/>
         <source>Multimedia</source>
-        <translation type="unfinished"></translation>
+        <translation>多媒体</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="474"/>
         <source>Deepines</source>
-        <translation type="unfinished"></translation>
+        <translation>深度</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="484"/>
         <source>Office automation</source>
-        <translation type="unfinished"></translation>
+        <translation>办公自动化</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="466"/>
         <source>Deepines Store</source>
-        <translation type="unfinished"></translation>
+        <translation>深度商店</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="467"/>
         <source>TextLabel</source>
-        <translation type="unfinished"></translation>
+        <translation>TextLabel</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="505"/>
         <source>{app_count} apps selected to install, click here to review them</source>
-        <translation type="unfinished"></translation>
+        <translation>已选择安装 {app_count} 个应用，点击此处查看</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="502"/>
         <source>Installed</source>
-        <translation type="unfinished"></translation>
+        <translation>已安装</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="503"/>
         <source>{app_count} app selected to install, click here to review it</source>
-        <translation type="unfinished"></translation>
+        <translation>已选择安装 {app_count} 个应用，点击此处查看</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="501"/>
         <source>Selected</source>
-        <translation type="unfinished"></translation>
+        <translation>已选中</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="500"/>
         <source>Select the apps to install</source>
-        <translation type="unfinished"></translation>
+        <translation>选择要安装的应用</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="513"/>
         <source>Deepines repository is not installed on your system,&lt;br&gt;Deepines Store needs this repository to work.&lt;br&gt;In the following link you will find the instructions to install it.&lt;br&gt;&lt;br&gt;&lt;a href=&apos;#&apos;&gt;deepinenespanol.org/repositorio/ | Copy link&lt;a/&gt;&lt;br&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>您的系统上未安装深度仓库，&lt;br&gt;深度商店需要此存储库才能工作。&lt;br&gt;在以下链接中，您将能找到安装说明。&lt;br&gt;&lt;br&gt;&lt;a href=&quot;#&quot;&gt; deepinenespanol.org/repositorio/ |复制链接&lt;/a&gt;&lt;a&gt;&lt;br&gt;&lt;/a&gt;</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="507"/>
         <source>Unable to establish connection with the server, &lt;br&gt;please check your internet connection.&lt;br&gt;If the problem persists, please contact us via Telegram &lt;br&gt;at @deepinenespanol.&lt;br&gt;&lt;br&gt;&lt;a href=&apos;#&apos;&gt;deepinenespanol.org | Copy link&lt;/a&gt;&lt;br&gt;Visit Deepin en Espanol for more information.</source>
-        <translation type="unfinished"></translation>
+        <translation>无法与服务器建立连接，&lt;br&gt;请检查您的互联网连接。&lt;br&gt;如果问题仍然存在，请通过 Telegram &lt;br&gt;@deepinenespanol 联系我们。&lt;br&gt;&lt;br&gt;&lt;a href=&quot;#&quot;&gt;deepinenespanol.org |复制链接&lt;/a&gt;&lt;br&gt;访问 Deepin en Espanol 了解更多信息。</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="494"/>
         <source>Minimize</source>
-        <translation type="unfinished"></translation>
+        <translation>最小化</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="495"/>
         <source>Zoom</source>
-        <translation type="unfinished"></translation>
+        <translation>缩放</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="496"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>关闭</translation>
     </message>
 </context>
 </TS>
