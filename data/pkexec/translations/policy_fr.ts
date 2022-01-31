04b1a00f2f06d08b0f5b470a902cfc2b47a9e440
J. Lavoie <j.lavoie@net-c.ca>
2022-01-31 23:45:36 +0100

    Added translation using Weblate (French)

---
+++ data/pkexec/translations/policy_fr.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>policy</name>
+    <message>
+        <location filename="deepines!message" line="0"/>
+        <source>Deepines Store requires authentication</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="deepines!description" line="0"/>
+        <source>Allows graphical installation of applications from the Deepines repository</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
