2e2d112f01acdcbfa4847a995aab9bf3e73e58f8
Allan Nordhøy <epost@anotheragency.no>
2022-02-01 22:08:34 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: Deepines Store/Polkit Action
    Translate-URL: https://hosted.weblate.org/projects/deepines-store/polkit-action/nb_NO/

--- data/pkexec/translations/policy_nb_NO.ts
+++ data/pkexec/translations/policy_nb_NO.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="deepines!message" line="0"/>
         <source>Deepines Store requires authentication</source>
-        <translation type="unfinished"></translation>
+        <translation>Deepines-butikken krever identitetsbekreftelse</translation>
     </message>
     <message>
         <location filename="deepines!description" line="0"/>
         <source>Allows graphical installation of applications from the Deepines repository</source>
-        <translation type="unfinished"></translation>
+        <translation>Tillater grafisk installasjon av programmer fra Deepines-pakkebrønnen</translation>
     </message>
 </context>
 </TS>
