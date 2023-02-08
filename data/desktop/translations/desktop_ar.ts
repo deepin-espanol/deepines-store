2133c3a314be39cc2cfac8f5be088f70afe244cd
Khaled Mahmoud <Khaled.q6@gmail.com>
2023-02-08 20:09:04 +0000

    Translated using Weblate (Arabic)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: Deepines Store/Desktop File
    Translate-URL: https://hosted.weblate.org/projects/deepines-store/desktop-file/ar/

--- data/desktop/translations/desktop_ar.ts
+++ data/desktop/translations/desktop_ar.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="Desktop Entry]GenericName" line="0"/>
         <source>App Store</source>
-        <translation type="unfinished"></translation>
+        <translation>متجر التطبيقات</translation>
     </message>
     <message>
         <location filename="Desktop Entry]Name" line="0"/>
         <source>Deepines Store</source>
-        <translation type="unfinished"></translation>
+        <translation>متجر ديبين</translation>
     </message>
     <message>
         <location filename="Desktop Entry]Comment" line="0"/>
         <source>Download and install applications from Deepines repository</source>
-        <translation type="unfinished"></translation>
+        <translation>حمل وثبت التطبيقات من مستودعات ديبين</translation>
     </message>
     <message>
         <location filename="Desktop Entry]Keywords" line="0"/>
         <source>store;app;apps;applications;install;program;software;</source>
-        <translation type="unfinished"></translation>
+        <translation>متجر;تطبيق;تطبيقات;برامج;تثبيت;حزم;برمجيات;</translation>
     </message>
 </context>
 </TS>
