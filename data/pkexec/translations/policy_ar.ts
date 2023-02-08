ec749e9a968e319a6e9834fd6ab8c5a5971024d1
Khaled Mahmoud <Khaled.q6@gmail.com>
2023-02-08 20:12:50 +0000

    Translated using Weblate (Arabic)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: Deepines Store/Polkit Action
    Translate-URL: https://hosted.weblate.org/projects/deepines-store/polkit-action/ar/

--- data/pkexec/translations/policy_ar.ts
+++ data/pkexec/translations/policy_ar.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="deepines!message" line="0"/>
         <source>Deepines Store requires authentication</source>
-        <translation type="unfinished"></translation>
+        <translation>يحتاج متجر ديبين لعملية مصادقة</translation>
     </message>
     <message>
         <location filename="deepines!description" line="0"/>
         <source>Allows graphical installation of applications from the Deepines repository</source>
-        <translation type="unfinished"></translation>
+        <translation>اسمح لواجهة التثبيت الرسومي للتطبيقات من مستودعات ديبين</translation>
     </message>
 </context>
 </TS>
