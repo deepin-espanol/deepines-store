5eb936601bb6bbb3969d5933ef7fc740f5999a2f
Khaled Mahmoud <Khaled.q6@gmail.com>
2023-02-08 20:02:04 +0000

    Translated using Weblate (Arabic)
    Currently translated at 100.0% (56 of 56 strings)
    
    Translation: Deepines Store/Deepines Store
    Translate-URL: https://hosted.weblate.org/projects/deepines-store/deepines-store/ar/

--- translations/ar.ts
+++ translations/ar.ts
@@ -6,67 +6,67 @@
     <message>
         <location filename="../deepinesStore/about.py" line="235"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>حول</translation>
     </message>
     <message>
         <location filename="../deepinesStore/about.py" line="236"/>
         <source>Deepines Store</source>
-        <translation type="unfinished"></translation>
+        <translation>متجر ديبين</translation>
     </message>
     <message>
         <location filename="../deepinesStore/about.py" line="237"/>
         <source>Developed by Deepin en Español</source>
-        <translation type="unfinished"></translation>
+        <translation>طورت بواسطة ديبين أسبانيا</translation>
     </message>
     <message>
         <location filename="../deepinesStore/about.py" line="238"/>
         <source>Project Leader: {}</source>
-        <translation type="unfinished"></translation>
+        <translation>قادت المشروع: {}</translation>
     </message>
     <message>
         <location filename="../deepinesStore/about.py" line="239"/>
         <source>Development: {}</source>
-        <translation type="unfinished"></translation>
+        <translation>التطوير: {}</translation>
     </message>
     <message>
         <location filename="../deepinesStore/about.py" line="240"/>
         <source>UI: {}</source>
-        <translation type="unfinished"></translation>
+        <translation>واجهة المستخدم: {}</translation>
     </message>
     <message>
         <location filename="../deepinesStore/about.py" line="241"/>
         <source>OG UX Writer: {}</source>
-        <translation type="unfinished"></translation>
+        <translation>OG كاتب تجربة المستخدم: {}</translation>
     </message>
     <message>
         <location filename="../deepinesStore/about.py" line="242"/>
         <source>Design: {}</source>
-        <translation type="unfinished"></translation>
+        <translation>التصميم: {}</translation>
     </message>
     <message>
         <location filename="../deepinesStore/about.py" line="243"/>
         <source>SysAdmin: {}</source>
-        <translation type="unfinished"></translation>
+        <translation>مدير النظام: {}</translation>
     </message>
     <message>
         <location filename="../deepinesStore/about.py" line="244"/>
         <source>Web: {}</source>
-        <translation type="unfinished"></translation>
+        <translation>الويب: {}</translation>
     </message>
     <message>
         <location filename="../deepinesStore/about.py" line="245"/>
         <source>Servers: {}</source>
-        <translation type="unfinished"></translation>
+        <translation>خوادم: {}</translation>
     </message>
     <message>
         <location filename="../deepinesStore/about.py" line="246"/>
         <source>Collaborators: {}</source>
-        <translation type="unfinished"></translation>
+        <translation>مساهمون: {}</translation>
     </message>
     <message>
         <location filename="../deepinesStore/about.py" line="247"/>
         <source>deepinenespañol.org | Copy link</source>
-        <translation type="unfinished"></translation>
+        <translation>deepinenespañol.org | انسخ الرابط</translation>
     </message>
 </context>
 <context>
@@ -76,17 +76,19 @@
         <source>
 Installing {item}
 </source>
-        <translation type="unfinished"></translation>
+        <translation>
+جاري تثبيت {item}
+</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="140"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>اغلق</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="139"/>
         <source>Retry</source>
-        <translation type="unfinished"></translation>
+        <translation>حاول مجددا</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="154"/>
@@ -96,19 +98,26 @@ The installation system is locked by
 another installation or upgrade process, 
 wait until it finishes and click Retry. 
 If you use Synaptic, make sure it is closed.</source>
-        <translation type="unfinished"></translation>
+        <translation>
+
+تم تقييد النظام المثبت بواسطة 
+مثبت اخر أو عملية ترقية, 
+انتظر حتى ينتهي ثم قم بالضغط على المحاولة مجددا. 
+اذا كنت تستخدم سينابتك قم باغلاقه.</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="138"/>
         <source>Install</source>
-        <translation type="unfinished"></translation>
+        <translation>ثبت</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="142"/>
         <source>
 All processes have been completed.
 </source>
-        <translation type="unfinished"></translation>
+        <translation>
+اكتملت جميع العمليات.
+</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="141"/>
@@ -116,12 +125,16 @@ All processes have been completed.
 
 Warning: do not close the window, interrupting the installation may damage your system.
 </source>
-        <translation type="unfinished"></translation>
+        <translation>
+
+تحذير: لا تغلق النافذة،
+إنهاء عملية التثبيت قد يدمر جهازك.
+</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="135"/>
         <source>Installation process - Deepines Store</source>
-        <translation type="unfinished"></translation>
+        <translation>عملية التثبيت - متجر ديبين</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="158"/>
@@ -129,14 +142,19 @@ Warning: do not close the window, interrupting the installation may damage your 
 The database is being updated.
 
 </source>
-        <translation type="unfinished"></translation>
+        <translation>
+حدثت قاعدة البيانات.
+
+</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="144"/>
         <source>
 The installation of {item} is finished.
 </source>
-        <translation type="unfinished"></translation>
+        <translation>
+تم الانتهاء من تثبيت {item}.
+</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="147"/>
@@ -146,19 +164,26 @@ The network connection has failed and the installation has not been completed.
 Check that your computer is connected to the Internet and click Retry.
 If the problem persists, send a report to t.me/deepinenespanol.
 </source>
-        <translation type="unfinished"></translation>
+        <translation>
+
+قطع الاتصال بالإنترنت ولم تستكمل عملية التثبيت.
+شيك على اتصال الانترنت ثم حاول مجددا.
+ان لم تحل المشكلة تواصل مع t.me/deepinenespanol.
+</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="136"/>
         <source>{app_count} app has been selected for installation:
 </source>
-        <translation type="unfinished"></translation>
+        <translation>تم تحديد {app_count} تطبيق للتثبيت:
+</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="137"/>
         <source>{app_count} apps have been selected for installation:
 </source>
-        <translation type="unfinished"></translation>
+        <translation>تم تحديد {app_count} تطبيق للتثبيت:
+</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="145"/>
@@ -167,7 +192,11 @@ If the problem persists, send a report to t.me/deepinenespanol.
 An error has occurred, please try again.
 If the problem persists, contact the administrator.
 </source>
-        <translation type="unfinished"></translation>
+        <translation>
+
+حدث خطأ، رجاء حاول مجددا.
+ان استمرت المشكلة اتصل بالمدير.
+</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="150"/>
@@ -178,7 +207,12 @@ they depend on other packages that cannot be installed.
 
 You can look for help in our forum at deepinenespanol.org/comunidad
 or in our Telegram group t.me/deepinenespanol.</source>
-        <translation type="unfinished"></translation>
+        <translation>
+
+تطبيق واحد أو أكثر لم تثبت بسبب
+انه يعتمد على حزم أخرى لا يمكن تثبيتها.
+ابحث عن حل في منتدياتنا على الرابط deepinenespanol.org/comunidad
+او في مجموعة التلغرام t.me/deepinenespanol.</translation>
     </message>
 </context>
 <context>
@@ -186,12 +220,12 @@ or in our Telegram group t.me/deepinenespanol.</source>
     <message>
         <location filename="../deepinesStore/cardg.py" line="70"/>
         <source>Install</source>
-        <translation type="unfinished"></translation>
+        <translation>ثبت</translation>
     </message>
     <message>
         <location filename="../deepinesStore/cardg.py" line="69"/>
         <source>Card</source>
-        <translation type="unfinished"></translation>
+        <translation>بطاقة</translation>
     </message>
 </context>
 <context>
@@ -199,133 +233,134 @@ or in our Telegram group t.me/deepinenespanol.</source>
     <message>
         <location filename="../deepinesStore/maing.py" line="467"/>
         <source>Home</source>
-        <translation type="unfinished"></translation>
+        <translation>الرئيسية</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="477"/>
         <source>Games</source>
-        <translation type="unfinished"></translation>
+        <translation>الالعاب</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="485"/>
         <source>Other</source>
-        <translation type="unfinished"></translation>
+        <translation>أخرى</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="463"/>
         <source>Install</source>
-        <translation type="unfinished"></translation>
+        <translation>ثبت</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="488"/>
         <source>About 
 Version: {version}</source>
-        <translation type="unfinished"></translation>
+        <translation>حول.
+الأصدار: {version}</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="481"/>
         <source>Development</source>
-        <translation type="unfinished"></translation>
+        <translation>التطوير</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="487"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>ابحث</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="471"/>
         <source>Internet</source>
-        <translation type="unfinished"></translation>
+        <translation>الانترنت</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="483"/>
         <source>System</source>
-        <translation type="unfinished"></translation>
+        <translation>النظام</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="475"/>
         <source>Graphics</source>
-        <translation type="unfinished"></translation>
+        <translation>الرسوميات</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="460"/>
         <source>About us</source>
-        <translation type="unfinished"></translation>
+        <translation>من نحن</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="473"/>
         <source>Multimedia</source>
-        <translation type="unfinished"></translation>
+        <translation>الملتميديا</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="469"/>
         <source>Deepines</source>
-        <translation type="unfinished"></translation>
+        <translation>ديبين</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="479"/>
         <source>Office automation</source>
-        <translation type="unfinished"></translation>
+        <translation>أتمتة المكاتب</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="461"/>
         <source>Deepines Store</source>
-        <translation type="unfinished"></translation>
+        <translation>متجر ديبين</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="462"/>
         <source>TextLabel</source>
-        <translation type="unfinished"></translation>
+        <translation>تسمية نصية</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="500"/>
         <source>{app_count} apps selected to install, click here to review them</source>
-        <translation type="unfinished"></translation>
+        <translation>تم تحديد {app_count} تطبيق، انقر هنا لرؤيتهم</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="497"/>
         <source>Installed</source>
-        <translation type="unfinished"></translation>
+        <translation>مثبت</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="498"/>
         <source>{app_count} app selected to install, click here to review it</source>
-        <translation type="unfinished"></translation>
+        <translation>تم تحديد {app_count} تطبيق لتثبيت، أنقر هنا لمشاهدتهم</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="496"/>
         <source>Selected</source>
-        <translation type="unfinished"></translation>
+        <translation>تحديد</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="495"/>
         <source>Select the apps to install</source>
-        <translation type="unfinished"></translation>
+        <translation>حدد التطبيقات لتثبيت</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="508"/>
         <source>Deepines repository is not installed on your system,&lt;br&gt;Deepines Store needs this repository to work.&lt;br&gt;In the following link you will find the instructions to install it.&lt;br&gt;&lt;br&gt;&lt;a href=&apos;#&apos;&gt;deepinenespanol.org/repositorio/ | Copy link&lt;a/&gt;&lt;br&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>لم تثبت مستودعات ديبين على نظامك، &lt;br&gt;يحتاج متجر ديبين لهذه المستودعات لتعمل.&lt;br&gt;على الرابط التالي سوف تجد التعليمات لتثبيتها. &lt;br&gt;&lt;br&gt;&lt;a href=&quot;#&quot;&gt;deepinenespanol.org/repositorio/ | Copy link&lt;/a&gt;&lt;a&gt;&lt;br&gt;&lt;/a&gt;</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="502"/>
         <source>Unable to establish connection with the server, &lt;br&gt;please check your internet connection.&lt;br&gt;If the problem persists, please contact us via Telegram &lt;br&gt;at @deepinenespanol.&lt;br&gt;&lt;br&gt;&lt;a href=&apos;#&apos;&gt;deepinenespanol.org | Copy link&lt;/a&gt;&lt;br&gt;Visit Deepin en Espanol for more information.</source>
-        <translation type="unfinished"></translation>
+        <translation>غير قادر على الاتصال بالسيرفر،&lt;br&gt;رجاء تفقد اتصالك بالإنترنت.&lt;br&gt; تواصل معنا على قناة التلغرام إن استمرت المشكلة&lt;br&gt;@deepinenespanol.&lt;br&gt;&lt;br&gt;&lt;a href=&quot;#&quot;&gt;deepinenespanol.org | انسخ الرابط&lt;/a&gt;&lt;br&gt;قم بزيارة ديبين إسبانيا لمزيد من المعلومات.</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="489"/>
         <source>Minimize</source>
-        <translation type="unfinished"></translation>
+        <translation>تصغير</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="490"/>
         <source>Zoom</source>
-        <translation type="unfinished"></translation>
+        <translation>تقريب</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="491"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>أغلق</translation>
     </message>
 </context>
 </TS>
