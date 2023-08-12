1c4cb15b5a9cb461bd02d5ab763124f187ee9e0d
yangyangdaji <1504305527@qq.com>
2023-08-12 09:44:46 +0000

    Translated using Weblate (Chinese (Simplified))
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: Deepines Store/Desktop File
    Translate-URL: https://hosted.weblate.org/projects/deepines-store/desktop-file/zh_Hans/

--- data/desktop/translations/desktop_zh_Hans.ts
+++ data/desktop/translations/desktop_zh_Hans.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="Desktop Entry]GenericName" line="0"/>
         <source>App Store</source>
-        <translation type="unfinished"></translation>
+        <translation>应用商店</translation>
     </message>
     <message>
         <location filename="Desktop Entry]Name" line="0"/>
         <source>Deepines Store</source>
-        <translation type="unfinished"></translation>
+        <translation>深度西班牙商店</translation>
     </message>
     <message>
         <location filename="Desktop Entry]Comment" line="0"/>
         <source>Download and install applications from Deepines repository</source>
-        <translation type="unfinished"></translation>
+        <translation>从 Deepines 存储库下载并安装应用</translation>
     </message>
     <message>
         <location filename="Desktop Entry]Keywords" line="0"/>
         <source>store;app;apps;applications;install;program;software;</source>
-        <translation type="unfinished"></translation>
+        <translation>store;app;apps;applications;install;program;software;</translation>
     </message>
 </context>
 </TS>
