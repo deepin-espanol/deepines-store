519ed83f872ddb16992e489278fd4aed9f4c680f
Béla Bajza <bajzabela@gmail.com>
2023-09-14 06:17:49 +0000

    Translated using Weblate (Hungarian)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: Deepines Store/Desktop File
    Translate-URL: https://hosted.weblate.org/projects/deepines-store/desktop-file/hu/

--- data/desktop/translations/desktop_hu.ts
+++ data/desktop/translations/desktop_hu.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="Desktop Entry]GenericName" line="0"/>
         <source>App Store</source>
-        <translation type="unfinished"></translation>
+        <translation>Áruház</translation>
     </message>
     <message>
         <location filename="Desktop Entry]Name" line="0"/>
         <source>Deepines Store</source>
-        <translation type="unfinished"></translation>
+        <translation>Deepines Áruház</translation>
     </message>
     <message>
         <location filename="Desktop Entry]Comment" line="0"/>
         <source>Download and install applications from Deepines repository</source>
-        <translation type="unfinished"></translation>
+        <translation>Töltse le és telepítse az alkalmazásokat a Deepines adattárból</translation>
     </message>
     <message>
         <location filename="Desktop Entry]Keywords" line="0"/>
         <source>store;app;apps;applications;install;program;software;</source>
-        <translation type="unfinished"></translation>
+        <translation>áruház;alkalmazás;alkalmazások;telepítés;program;szoftver;</translation>
     </message>
 </context>
 </TS>
