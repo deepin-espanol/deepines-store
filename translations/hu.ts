fc60b63606265a8662e8f66d894c93f62df04d57
Béla Bajza <bajzabela@gmail.com>
2023-09-14 05:14:18 +0000

    Translated using Weblate (Hungarian)
    Currently translated at 100.0% (56 of 56 strings)
    
    Translation: Deepines Store/Deepines Store
    Translate-URL: https://hosted.weblate.org/projects/deepines-store/deepines-store/hu/

--- translations/hu.ts
+++ translations/hu.ts
@@ -6,67 +6,67 @@
     <message>
         <location filename="../deepinesStore/about.py" line="235"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Rólunk</translation>
     </message>
     <message>
         <location filename="../deepinesStore/about.py" line="236"/>
         <source>Deepines Store</source>
-        <translation type="unfinished"></translation>
+        <translation>Deepines Áruház</translation>
     </message>
     <message>
         <location filename="../deepinesStore/about.py" line="237"/>
         <source>Developed by Deepin en Español</source>
-        <translation type="unfinished"></translation>
+        <translation>A Deepin Español közösség által fejlesztve</translation>
     </message>
     <message>
         <location filename="../deepinesStore/about.py" line="238"/>
         <source>Project Leader: {}</source>
-        <translation type="unfinished"></translation>
+        <translation>Projekt vezető: {}</translation>
     </message>
     <message>
         <location filename="../deepinesStore/about.py" line="239"/>
         <source>Development: {}</source>
-        <translation type="unfinished"></translation>
+        <translation>Fejlesztők: {}</translation>
     </message>
     <message>
         <location filename="../deepinesStore/about.py" line="240"/>
         <source>UI: {}</source>
-        <translation type="unfinished"></translation>
+        <translation>Felhasználói felület: {}</translation>
     </message>
     <message>
         <location filename="../deepinesStore/about.py" line="241"/>
         <source>OG UX Writer: {}</source>
-        <translation type="unfinished"></translation>
+        <translation>OG UX Író: {}</translation>
     </message>
     <message>
         <location filename="../deepinesStore/about.py" line="242"/>
         <source>Design: {}</source>
-        <translation type="unfinished"></translation>
+        <translation>Dizájn: {}</translation>
     </message>
     <message>
         <location filename="../deepinesStore/about.py" line="243"/>
         <source>SysAdmin: {}</source>
-        <translation type="unfinished"></translation>
+        <translation>Rendszer Adminisztrátor: {}</translation>
     </message>
     <message>
         <location filename="../deepinesStore/about.py" line="244"/>
         <source>Web: {}</source>
-        <translation type="unfinished"></translation>
+        <translation>Web: {}</translation>
     </message>
     <message>
         <location filename="../deepinesStore/about.py" line="245"/>
         <source>Servers: {}</source>
-        <translation type="unfinished"></translation>
+        <translation>Szerverek: {}</translation>
     </message>
     <message>
         <location filename="../deepinesStore/about.py" line="246"/>
         <source>Collaborators: {}</source>
-        <translation type="unfinished"></translation>
+        <translation>Együttműködők: {}</translation>
     </message>
     <message>
         <location filename="../deepinesStore/about.py" line="247"/>
         <source>deepinenespañol.org | Copy link</source>
-        <translation type="unfinished"></translation>
+        <translation>deepinenespañol.org | Link másolása</translation>
     </message>
 </context>
 <context>
@@ -76,17 +76,19 @@
         <source>
 Installing {item}
 </source>
-        <translation type="unfinished"></translation>
+        <translation>
+Telepítés {item}
+</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="140"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Bezárás</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="139"/>
         <source>Retry</source>
-        <translation type="unfinished"></translation>
+        <translation>Újra</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="154"/>
@@ -96,19 +98,25 @@ The installation system is locked by
 another installation or upgrade process, 
 wait until it finishes and click Retry. 
 If you use Synaptic, make sure it is closed.</source>
-        <translation type="unfinished"></translation>
+        <translation>
+
+A telepítési rendszert egy másik telepítési vagy frissítési folyamat zárolja,
+várja meg, amíg befejeződik, majd kattintson az Újra gombra.
+Ha Synaptic-ot használ, győződjön meg arról, hogy le van zárva.</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="138"/>
         <source>Install</source>
-        <translation type="unfinished"></translation>
+        <translation>Telepítés</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="142"/>
         <source>
 All processes have been completed.
 </source>
-        <translation type="unfinished"></translation>
+        <translation>
+Az összes folyamat sikeresen befejeződött
+</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="141"/>
@@ -116,12 +124,15 @@ All processes have been completed.
 
 Warning: do not close the window, interrupting the installation may damage your system.
 </source>
-        <translation type="unfinished"></translation>
+        <translation>
+
+Figyelmeztetés: ne zárja be az ablakot, mert a telepítés megszakítása károsíthatja a rendszert.
+</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="135"/>
         <source>Installation process - Deepines Store</source>
-        <translation type="unfinished"></translation>
+        <translation>Telepítési folyamat - Deepines Áruház</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="158"/>
@@ -129,14 +140,19 @@ Warning: do not close the window, interrupting the installation may damage your 
 The database is being updated.
 
 </source>
-        <translation type="unfinished"></translation>
+        <translation>
+Az adatbázis frissítése folyamatban.
+
+</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="144"/>
         <source>
 The installation of {item} is finished.
 </source>
-        <translation type="unfinished"></translation>
+        <translation>
+A(z) {item} telepítése befejeződött.
+</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="147"/>
@@ -146,19 +162,26 @@ The network connection has failed and the installation has not been completed.
 Check that your computer is connected to the Internet and click Retry.
 If the problem persists, send a report to t.me/deepinenespanol.
 </source>
-        <translation type="unfinished"></translation>
+        <translation>
+
+A hálózati kapcsolat meghiúsult, és a telepítés nem fejeződött be.
+Ellenőrizze, hogy a számítógépe csatlakozik-e az internethez, majd kattintson az Újra gombra.
+Ha a probléma továbbra is fennáll, küldjön jelentést a t.me/deepinenespanol címre.
+</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="136"/>
         <source>{app_count} app has been selected for installation:
 </source>
-        <translation type="unfinished"></translation>
+        <translation>{app_count} alkalmazás lett kiválasztva telepítésre:
+</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="137"/>
         <source>{app_count} apps have been selected for installation:
 </source>
-        <translation type="unfinished"></translation>
+        <translation>{app_count} alkalmazás lett kiválasztva telepítésre:
+</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="145"/>
@@ -167,7 +190,11 @@ If the problem persists, send a report to t.me/deepinenespanol.
 An error has occurred, please try again.
 If the problem persists, contact the administrator.
 </source>
-        <translation type="unfinished"></translation>
+        <translation>
+
+Hiba történt, kérjük próbálja újra.
+Ha a probléma továbbra is fennáll, forduljon a rendszergazdához.
+</translation>
     </message>
     <message>
         <location filename="../deepinesStore/dialog_install.py" line="150"/>
@@ -178,7 +205,13 @@ they depend on other packages that cannot be installed.
 
 You can look for help in our forum at deepinenespanol.org/comunidad
 or in our Telegram group t.me/deepinenespanol.</source>
-        <translation type="unfinished"></translation>
+        <translation>
+
+Egy vagy több alkalmazást nem sikerült telepíteni, mert
+más, nem telepíthető csomagoktól függenek.
+
+Segítséget kereshet fórumunkban a deepinenespanol.org/comunidad címen,
+vagy a Telegram csoportunkban t.me/deepinenespanol címen.</translation>
     </message>
 </context>
 <context>
@@ -186,12 +219,12 @@ or in our Telegram group t.me/deepinenespanol.</source>
     <message>
         <location filename="../deepinesStore/cardg.py" line="70"/>
         <source>Install</source>
-        <translation type="unfinished"></translation>
+        <translation>Telepítés</translation>
     </message>
     <message>
         <location filename="../deepinesStore/cardg.py" line="69"/>
         <source>Card</source>
-        <translation type="unfinished"></translation>
+        <translation>Kártya</translation>
     </message>
 </context>
 <context>
@@ -199,133 +232,134 @@ or in our Telegram group t.me/deepinenespanol.</source>
     <message>
         <location filename="../deepinesStore/maing.py" line="467"/>
         <source>Home</source>
-        <translation type="unfinished"></translation>
+        <translation>Kezdőlap</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="477"/>
         <source>Games</source>
-        <translation type="unfinished"></translation>
+        <translation>Játékok</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="485"/>
         <source>Other</source>
-        <translation type="unfinished"></translation>
+        <translation>Egyebek</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="463"/>
         <source>Install</source>
-        <translation type="unfinished"></translation>
+        <translation>Telepítés</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="488"/>
         <source>About 
 Version: {version}</source>
-        <translation type="unfinished"></translation>
+        <translation>Rólunk 
+Vezió: {version}</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="481"/>
         <source>Development</source>
-        <translation type="unfinished"></translation>
+        <translation>Fejlesztés</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="487"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Keresés</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="471"/>
         <source>Internet</source>
-        <translation type="unfinished"></translation>
+        <translation>Internet</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="483"/>
         <source>System</source>
-        <translation type="unfinished"></translation>
+        <translation>Rendszer</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="475"/>
         <source>Graphics</source>
-        <translation type="unfinished"></translation>
+        <translation>Grafika</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="460"/>
         <source>About us</source>
-        <translation type="unfinished"></translation>
+        <translation>Rólunk</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="473"/>
         <source>Multimedia</source>
-        <translation type="unfinished"></translation>
+        <translation>Multimédia</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="469"/>
         <source>Deepines</source>
-        <translation type="unfinished"></translation>
+        <translation>Deepines</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="479"/>
         <source>Office automation</source>
-        <translation type="unfinished"></translation>
+        <translation>Iroda automatizálás</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="461"/>
         <source>Deepines Store</source>
-        <translation type="unfinished"></translation>
+        <translation>Deepines Áruház</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="462"/>
         <source>TextLabel</source>
-        <translation type="unfinished"></translation>
+        <translation>Szöveg Címke</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="500"/>
         <source>{app_count} apps selected to install, click here to review them</source>
-        <translation type="unfinished"></translation>
+        <translation>{app_count} alkalmazás van kiválasztva telepítésre. Kattintson ide az áttekintésükhöz</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="497"/>
         <source>Installed</source>
-        <translation type="unfinished"></translation>
+        <translation>Telepítve</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="498"/>
         <source>{app_count} app selected to install, click here to review it</source>
-        <translation type="unfinished"></translation>
+        <translation>{app_count} alkalmazás van kiválasztva telepítésre. Kattintson ide az áttekintéshez</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="496"/>
         <source>Selected</source>
-        <translation type="unfinished"></translation>
+        <translation>Kiválasztva</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="495"/>
         <source>Select the apps to install</source>
-        <translation type="unfinished"></translation>
+        <translation>Válassza ki a telepíteni kívánt alkalmazásokat</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="508"/>
         <source>Deepines repository is not installed on your system,&lt;br&gt;Deepines Store needs this repository to work.&lt;br&gt;In the following link you will find the instructions to install it.&lt;br&gt;&lt;br&gt;&lt;a href=&apos;#&apos;&gt;deepinenespanol.org/repositorio/ | Copy link&lt;a/&gt;&lt;br&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>A Deepines tároló nincs telepítve az Ön rendszerére,&lt;br&gt;A Deepines Áruház működéséhez szüksége van erre a tárolóra.&lt;br&gt;A következő linken megtalálja a telepítéshez szükséges utasításokat.&lt;br&gt;&lt;br&gt;&lt;a href=&quot;#&quot;&gt; deepinenespanol.org/repositorio/ | Link másolása&lt;/a&gt;&lt;a&gt;&lt;br&gt;&lt;/a&gt;</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="502"/>
         <source>Unable to establish connection with the server, &lt;br&gt;please check your internet connection.&lt;br&gt;If the problem persists, please contact us via Telegram &lt;br&gt;at @deepinenespanol.&lt;br&gt;&lt;br&gt;&lt;a href=&apos;#&apos;&gt;deepinenespanol.org | Copy link&lt;/a&gt;&lt;br&gt;Visit Deepin en Espanol for more information.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nem sikerült kapcsolatot létesíteni a szerverrel, &lt;br&gt;kérjük ellenőrizze az internetkapcsolatát.&lt;br&gt;Ha a probléma továbbra is fennáll, kérjük lépjen kapcsolatba velünk a Telegramon keresztül &lt;br&gt;a @deepinenespanol címen.&lt;br&gt;&lt;br&gt;&lt;a href=&quot;#&quot;&gt;deepinenespanol.org | Link másolása&lt;/a&gt;&lt;br&gt;További információért keresse fel a Deepin en Espanol oldalt.</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="489"/>
         <source>Minimize</source>
-        <translation type="unfinished"></translation>
+        <translation>Kis méret</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="490"/>
         <source>Zoom</source>
-        <translation type="unfinished"></translation>
+        <translation>Nagyítás</translation>
     </message>
     <message>
         <location filename="../deepinesStore/maing.py" line="491"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Bezárás</translation>
     </message>
 </context>
 </TS>
