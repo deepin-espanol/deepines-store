dbc30503ba81c03965a1870d4938b8f7efca24da
Amaro Martínez <xoas@airmail.cc>
2024-08-03 04:30:40 -0500

    Update Flatpak AppStream for first time setup

--- store-setup.sh
+++ store-setup.sh
@@ -254,6 +254,7 @@ InstallDeepines() {
 InstallFlathubRepository() {
 	$Fmt "${INSTALLING_FLATHUB}" && FlathubInstallationResult=$(
 		flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+		flatpak update --appstream --assumeyes
 	) || {
 		$Fmt "$INSTALLING_FAILED"
 		echo "$FlathubInstallationResult"
