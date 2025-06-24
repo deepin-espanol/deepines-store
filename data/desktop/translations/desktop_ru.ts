4d6063689d2e7fb9314f6ae2faeb64ba89b56b4c
Maksim_220 Кабанов <law820314@gmail.com>
2025-06-24 17:02:22 +0200

    Translated using Weblate (Russian)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: Deepines Store/Desktop File
    Translate-URL: https://hosted.weblate.org/projects/deepines-store/desktop-file/ru/

--- data/desktop/translations/desktop_ru.ts
+++ data/desktop/translations/desktop_ru.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="Desktop Entry]GenericName" line="0"/>
         <source>App Store</source>
-        <translation type="unfinished"></translation>
+        <translation>App Store</translation>
     </message>
     <message>
         <location filename="Desktop Entry]Name" line="0"/>
         <source>Deepines Store</source>
-        <translation type="unfinished"></translation>
+        <translation>Магазин Deepines</translation>
     </message>
     <message>
         <location filename="Desktop Entry]Comment" line="0"/>
         <source>Download and install applications from Deepines repository</source>
-        <translation type="unfinished"></translation>
+        <translation>Загрузка и установка приложений из репозитория Deepines</translation>
     </message>
     <message>
         <location filename="Desktop Entry]Keywords" line="0"/>
         <source>store;app;apps;applications;install;program;software;</source>
-        <translation type="unfinished"></translation>
+        <translation>store;app;apps;applications;install;program;software;</translation>
     </message>
 </context>
 </TS>
