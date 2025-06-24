6bd0e293b1994117ab02f0243a0cd3cfee463530
Maksim_220 Кабанов <law820314@gmail.com>
2025-06-24 16:51:04 +0200

    Translated using Weblate (Russian)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: Deepines Store/Polkit Action
    Translate-URL: https://hosted.weblate.org/projects/deepines-store/polkit-action/ru/

--- data/pkexec/translations/policy_ru.ts
+++ data/pkexec/translations/policy_ru.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="deepines!message" line="0"/>
         <source>Deepines Store requires authentication</source>
-        <translation type="unfinished"></translation>
+        <translation>Deepines Store требует аутентификации</translation>
     </message>
     <message>
         <location filename="deepines!description" line="0"/>
         <source>Allows graphical installation of applications from the Deepines repository</source>
-        <translation type="unfinished"></translation>
+        <translation>Позволяет устанавливать приложения из репозитория Deepines в графическом режиме</translation>
     </message>
 </context>
 </TS>
