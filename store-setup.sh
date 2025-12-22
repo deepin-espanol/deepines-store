20bfe07ba5ea582460dbdc9d45bb0feff9ea7bf7
Amaro Martínez <xoas@airmail.cc>
2025-12-22 14:53:52 -0500

    Move maintainer scripts and control to separate files
    Refactored the packaging process by moving the preinst, postinst, postrm, and control template content from inline shell variables to dedicated files under data/control/. Updated store-setup.sh to use these files when generating the DEBIAN directory, improving maintainability and clarity.

--- store-setup.sh
+++ store-setup.sh
@@ -73,251 +73,6 @@ SetVariables() {
     WORK_DIR="$TEMP_DIR/$PKG_FULL_NAME"
 }
 
-# shellcheck disable=SC2016
-PREINSTSCRIPT='#!/bin/sh
-
-set -e
-
-CNFMOD="/usr/share/debconf/confmodule"
-if [ -f "${CNFMOD}" ]; then
-	. "${CNFMOD}"
-fi
-
-SEPARATOR="=============================================================="
-COMPATIBLE_VERSIONS="Deepines Store is only compatible with Deepin 20, Deepin 23 and Deepin 25"
-WE_RECOMMEND="  We recommend installing a recent version of Deepin. If you
-  are using Deepin 20, check that the configuration of the
-  repositories does not contain errors."
-
-case "${LANGUAGE:-$LANG}" in
-es*)
-	COMPATIBLE_VERSIONS="Tienda Deepines sólo es compatible con Deepin 20, Deepin 23 y Deepin 25"
-	WE_RECOMMEND=" Recomendamos instalar una versión reciente de Deepin. Si está
- usando Deepin 20, compruebe que la configuración de 
- los repositorios no contiene errores."
-	;;
-esac
-
-UnsupportedOS() {
-	echo >&2 "$SEPARATOR"
-	echo >&2 "$COMPATIBLE_VERSIONS"
-	echo >&2
-	echo >&2 "$WE_RECOMMEND"
-	echo >&2 "$SEPARATOR"
-	db_go || true
-	exit 1
-}
-
-CheckSupportedOS() {
-	DIST_ID=$(lsb_release -is)
-	REL_NUM=$(lsb_release -rs)
-	if [ "$DIST_ID" = "Deepin" ]; then
-		case $REL_NUM in
-		20 | 20.* | "23 Nightly" | 23 | 23.* | 25 | 25.*) ;;
-		*) UnsupportedOS ;;
-		esac
-	else
-		UnsupportedOS
-	fi
-}
-
-case "$1" in
-install | upgrade)
-	CheckSupportedOS
-	;;
-esac
-
-exit 0
-'
-
-# shellcheck disable=SC2016
-POSTINSTSCRIPT='#!/bin/sh
-
-set -e
-
-CNFMOD="/usr/share/debconf/confmodule"
-if [ -f "${CNFMOD}" ]; then
-	. "${CNFMOD}"
-fi
-
-Fmt="printf"
-
-mkdir -p /usr/share/desktop-directories
-
-SOURCES_DIR=/etc/apt/sources.list.d
-REPO="deb http://repositorio.deepines.com/pub/deepines/%d/ stable main"
-
-INSTALLING_DEEPINES="Installing Deepines %d repository and key..."
-INSTALLING_FLATHUB="Adding Flathub repository..."
-INSTALLING_DONE=" done.\n"
-INSTALLING_FAILED=" failed:\n"
-
-case "${LANGUAGE:-$LANG}" in
-es*)
-	INSTALLING_DEEPINES="Instalando repositorio y clave de Deepines %d..."
-	INSTALLING_FLATHUB="Añadiendo repositorio de Flathub..."
-	INSTALLING_DONE=" hecho.\n"
-	INSTALLING_FAILED=" falló:\n"
-	;;
-esac
-
-TRUSTED_DIR=/etc/apt/trusted.gpg.d
-SOURCES_DIR=/etc/apt/sources.list.d
-DEEPINES_LIST=$SOURCES_DIR/deepines.list
-DEEPINES_KEY=$TRUSTED_DIR/deepines.asc
-
-MakeRepoKey() {
-	cat <<EOF
------BEGIN PGP PUBLIC KEY BLOCK-----
-Version: GnuPG v1
-
-mQINBFrVFSQBEACfnCeQxQboQwrvHGRLLWc1fRNHhwp5+3dwxSlfERJfXrB1gSg1
-OjEq78eISG71jFiy3H16GvtFdiQfeVKIFFrYqY9144o++vwkh5cGRrJ+nunozZIq
-wvJGq+LjzQXc5mjidf31EPyCun5ybNuqO2oeEqYBits/zYN6fZTzBS55aGpMMNZ4
-Qn4njhW+w4TQ5rHqkJaVogctcDjCks+PTSIFCkYurz1qMLKqF04toMXqkHncPS+E
-3ykq/BdqeqiKGjD5desoHoWYN4hDqFhyd1smkpy3xSMLiqw/VRvotwYgX0eLUDLJ
-la8cxw73wiYLmIg2pHqDmKdHKiNjrolF+aZ2W7QS/JhgErBvSQA3k0pl7WEcRvlb
-Nb4vWI1/xFKpyNpHk8ey1g/2uQnKDFuPBE10uy01k4RaJPjzALjJU3oqtw8LCPhQ
-JECote1r/XhTCRWa3yQOdjOZgkW+KFPdSSoStFs11hWrFu0QGCt1LdssEMpC0pif
-hr3s2uK1GstMV7Vw15318bFU5WAEUpge4yuuwNl9yVF1F341B1datI0+7WkAuJy4
-+87qWpa1qzI2CkNGr9UTnme6tJVFGceL37oWahAUNysWkk3kxJkqcydTDT+7L6gj
-Kkyc8WQ6t4nIz2yrweV30ALqahXdsRJGnOy8h19RNA78w/w6zdLwArP4vwARAQAB
-tCVEZWVwaW5lcyA8ZGVlcGlubWlycm9ycGx1c0BnbWFpbC5jb20+iQI4BBMBAgAi
-BQJa1RUkAhsDBgsJCAcDAgYVCAIJCgsEFgIDAQIeAQIXgAAKCRDj1nq8WeItsMzR
-D/9XuUhFCFabRtFrDfQ23q8SNtXP+NeZZm1e02PlwrbZjPTIHim/0vUZhOz63h7g
-G++MreQzyvnlqFRk5KMQgzynn6j0lWu2xi4Tk2GqzyS+5nzp+dEzo/xIos0Phbbm
-hfaTyZ5PEYkLgXXKsEvsMajKMoJc3ClytHkx9PA08vZG79Wdzr0bTzlC7OYixGSS
-B9QDyvPnojEbiZc4GCF/+JFHQOUMvgGww+94AswN7J+mAMnF/bjgn/QnemKJ7ds2
-BSmTuJni/HzOvG+QG/VXhj147BoIhWtVZ6sAAQkaY0udWbv6QALs3gVBW/TP+V9E
-Jf067F1JWZK2Tevj1xLSs0NM7y6EiK6t1KUpve9TndB91ZXNpU88rviKUL4RCRkm
-qLvWb2oNOPD56jv/pAovsX6GRuq048Sv/K48BeouyzH7hIA+qCYSlafWgNfDkPNd
-ZlQ4pKfbaugllXfqFlqhnKWhMULyYH26RJSR6Dcd2y5edYw1OpcFyN5bRfuLN9i4
-oRO2KKxWmPx0nGKDeS5VGfkkANdyxZxbVFoBGv6RfFUp/qruO9ks00NqH9gGAz73
-r4XjqV2Dhr1oc+anBYyJAv0VhOhGdfXQl9OqWzeMaeMFCJqBD6hzhMAxhjkHCyNS
-OQCeW/A8zlm7zSCUiJho/cBkh4lBPITwGXbBXxmqXmCR97kCDQRa1RUkARAAz3AA
-n74JFazQzGPFFOF8N29g3dGSXULDOXYQfArEggbO4/E5cWWSfzpjCHVlKrbzHaXi
-4XQqidK+EcgaULNRsqs6PQ+yaXZz1kTV0XS+JdflakWokAGtlWveUrtQDTrFZOcT
-3D/wlrHid5ZmjlBdObESu3AJ4HKKrKkZvbR2e7P1o8VmiOYxK3QeyRmjrxfpTEMk
-ZWjfpgC9qZnTWUQ8lIbGGNH5G3tIWQpXPrNNaMxBXAIdKWdL9aGbeRIsd/qOu+dw
-aY2v9PcRC7XuD8esSZPcRmHn1pCfnlk4Utf5Z1zqO/pqV7gJPkiN7lt9yk/hAAK9
-z+4bwlBS3KWa36O2jqVBX9cAr9ys4bPOUqalv4p5LroYx26j0cjti5ILvqRXABny
-h0zfalueIv8Cw8V94XKMgfTEmPKleoWeeEpOaNO/R1z72dyX2JrhcRC4/Aq/Hgtt
-MWkoO/VPu46n/QsNiXKqqjsg0j88FrkMexYsZbFt+KoJGPHUt0FYde4D8pZlI8Od
-RdQJDQBadl6eOgzY+Az8LkfafYRUiZfWek9XkNUNtxtF8TgeZP4jm0UXvhVQemG+
-jo2+qsDO5KORj7mBsnC4gKGSwQlly1BJkbZXo7ScZhAVCxSHGa4WAFfyL0ZJZjp7
-qLBBdbW9SY9M1UKt7lEJSLtuXtljuzbIQimone8AEQEAAYkCHwQYAQIACQUCWtUV
-JAIbDAAKCRDj1nq8WeItsLo2EACdY3z3nkuRivm2zUpQznHgYSIBbZlrQrE3pAwl
-UrTJkU7iNiY++PHxYmmeIbUFEJtOZBsZ8qKxWG/nou5Ab+qUKllx/uZ21AqxfIXR
-3Qqp18ix2lYbc2/OlIENYEVVbF5zqUG+ZZl4/hIEsu+ANWViULqA602VcVOFud6j
-2rf0Z4Bp/V1A3Lic8Pfndug3NDWuS9g/5VND+LJ5nx438igNSCwqXNitXwgrI2D3
-DJHzZLBrRa3Tf4F5dWMMKEnJV5MMLEVLMe/DZ98dxfLp/S3edXX/+cpXeMUKM4yK
-HgeKnFk3IrrV2GrlK5QyWSHIFgkpV2lr1LjICYU2DLRECZxHrTp4e9kjEhVHMCWz
-k5W2btAHTSzYR8EZkstf7UCiz3La5h6EWFXKwaGQ1+QpuURlu+dSMm7oD/XLBx9G
-0c7xvL9N5cuXEcmyNsvglBNnIVkafSHXBeLFDIGKuGfqdFRXc6tXvCom5JSNjwQB
-WQSzPfoOPG5ryHhzHuAYZbp5aYSPBbxK+bVLZJY5nf5p0Ka4dxeHICqcbf1HP8td
-HBHo8LjV6u/uFnirPO7T1OGa+EVi06McnbQx25/7cFGGLTvf7mFxq++Fr1iu+Xd+
-7OVX4+r+uLPxkTEsBfM1R5FrOwLothtwEmd5AEY/KsJ3ihXxILIP4JFcUjFJweEy
-TGGtKg==
-=ym7H
------END PGP PUBLIC KEY BLOCK-----
-EOF
-}
-
-InstallFiles() {
-	REPO="deb http://repositorio.deepines.com/pub/deepines/%d/ stable main"
-	mkdir -p $TRUSTED_DIR
-	mkdir -p $SOURCES_DIR
-	$Fmt "${REPO}\n" "$1" >$DEEPINES_LIST
-	MakeRepoKey >$DEEPINES_KEY
-}
-
-InstallDeepinesRepository() {
-	$Fmt "${INSTALLING_DEEPINES}" "$1" && RepoInstallationResult=$(
-		InstallFiles "$1" 2>&1
-	) || {
-		$Fmt "$INSTALLING_FAILED"
-		echo "$RepoInstallationResult"
-		db_go || true
-		exit 1
-	} && $Fmt "$INSTALLING_DONE"
-}
-
-InstallDeepines() {
-	REL_NUM=$(lsb_release -rs)
-	case $REL_NUM in
-	20 | 20.*) InstallDeepinesRepository 4 ;;
-	"23 Nightly" | 23 | 23.*) InstallDeepinesRepository 5 ;;
-	25 | 25.*) InstallDeepinesRepository 6 ;;
-	esac
-}
-
-InstallFlathubRepository() {
-	$Fmt "${INSTALLING_FLATHUB}" && FlathubInstallationResult=$(
-		flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-		flatpak update --appstream --assumeyes
-		flatpak install flathub org.gtk.Gtk3theme.deepin org.gtk.Gtk3theme.deepin-dark --assumeyes
-	) || {
-		$Fmt "$INSTALLING_FAILED"
-		echo "$FlathubInstallationResult"
-		db_go || true
-		exit 1
-	} && $Fmt "$INSTALLING_DONE"
-}
-
-case "$1" in
-configure)
-	InstallDeepines
-	InstallFlathubRepository
-	;;
-esac
-
-exit 0
-'
-
-# shellcheck disable=SC2016
-POSTRMSCRIPT='#!/bin/sh
-
-set -e
-
-CNFMOD="/usr/share/debconf/confmodule"
-if [ -f "${CNFMOD}" ]; then
-	. "${CNFMOD}"
-fi
-
-DeleteRepository() {
-	APT_KEY="/etc/apt/trusted.gpg.d/deepines.asc"
-	APT_LIST="/etc/apt/sources.list.d/deepines.list"
-	rm -f "${APT_KEY}" "${APT_LIST}"
-}
-
-PurgeAppData() {
-	# Remove user-downloaded banners...
-	for homedir in /home/*; do
-		cfg="$homedir/.config/deepines-store"
-		[ -d "$cfg" ] || continue
-		echo "Removing $cfg"
-		rm -rf "$cfg"
-	done
-}
-
-case "$1" in
-remove | abort-upgrade)
-	DeleteRepository
-	db_purge
-	;;
-purge)
-	DeleteRepository
-	PurgeAppData
-	db_purge
-	;;
-abort-install)
-	db_purge
-	;;
-esac
-
-exit 0
-'
-
 ShowDetails() {
     echo "Packaging details:
 Name: $PKG_NAME
@@ -397,16 +152,16 @@ mkdir -p usr/share/polkit-1/actions
 deepin-policy-ts-convert ts2policy "$SH_DIR/data/pkexec/org.freedesktop.deepines.policy" \
     "$SH_DIR/data/pkexec/translations" usr/share/polkit-1/actions/org.freedesktop.deepines.policy
 
-echo "Generating 'preinst' script..."
-printf "%s" "$PREINSTSCRIPT" >DEBIAN/preinst
+echo "Writing 'preinst' script..."
+cat "$SH_DIR/data/control/preinst" >DEBIAN/preinst
 chmod 755 DEBIAN/preinst
 
-echo "Generating 'postinst' script..."
-printf "%s" "$POSTINSTSCRIPT" >DEBIAN/postinst
+echo "Writing 'postinst' script..."
+cat "$SH_DIR/data/control/postinst" >DEBIAN/postinst
 chmod 755 DEBIAN/postinst
 
-echo "Generating 'postrm' script..."
-printf "%s" "$POSTRMSCRIPT" >DEBIAN/postrm
+echo "Writing 'postrm' script..."
+cat "$SH_DIR/data/control/postrm" >DEBIAN/postrm
 chmod 755 DEBIAN/postrm
 
 YEAR="2025"
@@ -469,28 +224,15 @@ MakeManPage >usr/share/man/man1/deepines.1.gz
 find . -not \( -path ./DEBIAN -prune \) -type f -exec md5sum {} \; |
     sed "s|\./||" >DEBIAN/md5sums
 
-# Generate 'Installed-Size' variable.
+# Get 'Installed-Size' variable (used in GenerateControl).
+# shellcheck disable=SC2034
 INSIZE=$(du -s --exclude='DEBIAN/*' | grep -Eo "[0-9]*")
 Open "./DEBIAN/" # (-m) Manually update preinst, postinst, etc.
 
-P3=python3
-
 GenerateControl() {
-    cat <<EOF
-Package: $PKG_NAME
-Version: $PKG_VER
-Architecture: $PKG_ARCH
-Installed-Size: $INSIZE
-Section: admin
-Maintainer: $PKG_DEV
-Homepage: $PKG_SRC
-Priority: optional
-Pre-Depends: debconf (>= 0.5)
-Depends: $P3, $P3-lxml, $P3-pyqt5, $P3-requests, $P3-apt, flatpak, x11-utils
-Replaces: deepines-repository (<= 1:4.1), deepines-store:amd64 (<= 1.3.3)
-Description: Deepines repository, key and Store
- Deepines unofficial repository and Store by deepinenespanol.org
-EOF
+    eval "cat <<EOF
+$(cat "$SH_DIR/data/control/control.in")
+EOF"
 }
 
 echo "Generating control file..."
