3a261f173f288aacd5d056010fc27719e67a2ac3
தமிழ்நேரம் <tamilneram247@gmail.com>
2026-06-24 18:37:35 +0200

    Translated using Weblate (Tamil)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: Deepines Store/Desktop File
    Translate-URL: https://hosted.weblate.org/projects/deepines-store/desktop-file/ta/

--- data/desktop/translations/desktop_ta.ts
+++ data/desktop/translations/desktop_ta.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="Desktop Entry]GenericName" line="0"/>
         <source>App Store</source>
-        <translation type="unfinished"></translation>
+        <translation>ஆப் கடை</translation>
     </message>
     <message>
         <location filename="Desktop Entry]Name" line="0"/>
         <source>Deepines Store</source>
-        <translation type="unfinished"></translation>
+        <translation>டீபைன்ச் கடை</translation>
     </message>
     <message>
         <location filename="Desktop Entry]Comment" line="0"/>
         <source>Download and install applications from Deepines repository</source>
-        <translation type="unfinished"></translation>
+        <translation>Deepines களஞ்சியத்திலிருந்து பயன்பாடுகளைப் பதிவிறக்கி நிறுவவும்</translation>
     </message>
     <message>
         <location filename="Desktop Entry]Keywords" line="0"/>
         <source>store;app;apps;applications;install;program;software;</source>
-        <translation type="unfinished"></translation>
+        <translation>ச்டோர்;ஆப்;ஆப்ச்;பயன்பாடுகள்;நிறுவு;நிரல்;மென்பொருள்;</translation>
     </message>
 </context>
 </TS>
